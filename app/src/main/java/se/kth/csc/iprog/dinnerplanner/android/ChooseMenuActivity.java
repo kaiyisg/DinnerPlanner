39542d28544daddf85ad678e98a44c76b816f09c
Tan Fu Nan <Funy@Tans-MacBook-Pro-4.local>
2016-02-23 21:50:29 +0100

    This is the latest from my computer

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/ChooseMenuActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/ChooseMenuActivity.java
@@ -1,7 +1,10 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.ChooseMenuBgView;
 import se.kth.csc.iprog.dinnerplanner.android.view.ChoosingDishesView;
@@ -9,10 +12,13 @@ import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 
 public class ChooseMenuActivity extends Activity {
 
+    Button createDinnerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_menu);
+        this.getActionBar().setTitle("DinnerPlanner");
 
         ChooseMenuBgView chooseMenuBgView = new ChooseMenuBgView(findViewById(R.id.choose_menu_bg_view_id));
 
@@ -22,6 +28,18 @@ public class ChooseMenuActivity extends Activity {
         ChoosingDishesView choosingStartersView = new ChoosingDishesView(findViewById(R.id.starter_choosing_dishes_id));
         ChoosingDishesView choosingMainsView = new ChoosingDishesView(findViewById(R.id.main_choosing_dishes_id));
         ChoosingDishesView choosingDessertsView = new ChoosingDishesView(findViewById(R.id.dessert_choosing_dishes_id));
+
+        createDinnerButton.setOnClickListener(
+
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), ViewRecipeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
 
 
     }
