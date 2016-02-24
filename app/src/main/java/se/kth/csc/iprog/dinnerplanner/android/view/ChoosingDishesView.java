273f7e62a8f7614d56178293665324a4181d820c
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-02-24 12:23:24 +0100

    images successfully parsed

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/ChoosingDishesView.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/ChoosingDishesView.java
@@ -1,9 +1,12 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -19,21 +22,24 @@ import se.kth.csc.iprog.dinnerplanner.model.Dish;
 public class ChoosingDishesView {
 
     public static final String drawable = "@drawable/";
-
+    HorizontalScrollView horizontalScrollView;
+    TextView dishCategory;
     View view;
 
     public ChoosingDishesView(View view, int dishType, DinnerModel model){
 
         this.view = view;
 
-
-
-        TextView dishCategory = (TextView)view.findViewById(R.id.textView9);
+        horizontalScrollView = (HorizontalScrollView)view.findViewById(R.id.horizontalScrollView);
+        dishCategory = (TextView)view.findViewById(R.id.textView9);
 
         if(dishType == Dish.STARTER){
 
             dishCategory.setText("Starter");
             Set<Dish> starterDishes =  model.getDishesOfType(Dish.STARTER);
+
+            LinearLayout topLinearLayout = new LinearLayout(view.getContext());
+            topLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
 
             for(Dish d : starterDishes){
 
@@ -42,62 +48,22 @@ public class ChoosingDishesView {
                 String dishName = d.getName();
                 double dishCost = d.getPrice();
 
-                LinearLayout dishesView = (LinearLayout)view.findViewById(R.id.starter_choosing_dishes_id);
+                LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View dishView = inflater.inflate(R.layout.food_icon, null);
 
-
-                /*
-                //method of defining a new xml layout and resizing from there
-                LinearLayout dishView = (LinearLayout)view.findViewById(R.id.food_icon_id);
-
-                ImageButton imageButton = (ImageButton)view.findViewById(R.id.imageButton5);
-                int imageResource = view.getResources().getIdentifier(dishImageUri, null, view.getContext().getPackageName());
-                Drawable res = view.getContext().getResources().getDrawable(imageResource);
+                ImageButton imageButton = (ImageButton)dishView.findViewById(R.id.imageButton5);
+                int imageResource = dishView.getResources().getIdentifier(dishImageUri, null, dishView.getContext().getPackageName());
+                Drawable res = dishView.getContext().getResources().getDrawable(imageResource);
                 imageButton.setImageDrawable(res);
 
-                TextView dishText = (TextView)view.findViewById(R.id.textView20);
+                TextView dishText = (TextView)dishView.findViewById(R.id.textView20);
                 dishText.setText(dishName);
 
-                dishesView.addView(dishView);
-                */
-
-
-
-                //method of defining everything dynamically
-
-                LinearLayout container = new LinearLayout(view.getContext());
-                container.setOrientation(LinearLayout.HORIZONTAL);
-
-                FrameLayout imageholder = new FrameLayout(view.getContext());
-                container.addView(imageholder);
-                //imageholder.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                 //       ViewGroup.LayoutParams.MATCH_PARENT));
-                       // 30, 30));
-
-                ImageButton imageButton = new ImageButton(view.getContext());
-                imageholder.addView(imageButton);
-                int imageResource = view.getResources().getIdentifier(dishImageUri, null, view.getContext().getPackageName());
-                Drawable res = view.getContext().getResources().getDrawable(imageResource);
-                imageButton.setImageDrawable(res);
-                //imageButton.setLayoutParams(new ViewGroup.LayoutParams(
-                //        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                //imageButton.requestLayout();
-
-                TextView dishText = new TextView(view.getContext());
-                dishText.setText(dishName);
-                container.addView(dishText);
-
-                dishesView.addView(container);
-
-
-                //another method for not sure what
-                /*
-                int imageResource = view.getResources().getIdentifier(dishImageUri, null, view.getContext().getPackageName());
-                ImageButton imageButton= (ImageButton)view.findViewById(R.id.imageButton5);
-                Drawable res = view.getContext().getResources().getDrawable(imageResource);
-                imageButton.setImageDrawable(res);
-                */
+                topLinearLayout.addView(dishView);
 
             }
+
+            horizontalScrollView.addView(topLinearLayout);
 
         }else if(dishType == Dish.MAIN){
 
