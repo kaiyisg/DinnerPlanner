173e07218429a574aa69898701d235730f6506a4
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-02-24 11:11:07 +0100

    experimenting adding images

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/ChoosingDishesView.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/ChoosingDishesView.java
@@ -2,8 +2,11 @@ package se.kth.csc.iprog.dinnerplanner.android.view;
 
 
 import android.graphics.drawable.Drawable;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View;
 
@@ -23,19 +26,9 @@ public class ChoosingDishesView {
 
         this.view = view;
 
+
+
         TextView dishCategory = (TextView)view.findViewById(R.id.textView9);
-
-        //example code if you have a component (etc image view) that you want to pass the image over,
-        //the know how to load images dynamically
-		/*
-		ImageView img = //You either create new or find from layout
-		int drawableResId = DinnerPlannerApplication.getDrawable(view.getContext(),"drawable_name_without_extension");
-		img.setImageResource(drawableResId);
-		 */
-
-        //Note: be sure to not have extension in the drawable name. If you are using the getImage() from the DinnerModel
-        // this will give you the image name with .jpg extension. So either modify the model, or be sure to remove the
-        // extension before you pass it to the getDrawable() method.
 
         if(dishType == Dish.STARTER){
 
@@ -43,15 +36,66 @@ public class ChoosingDishesView {
             Set<Dish> starterDishes =  model.getDishesOfType(Dish.STARTER);
 
             for(Dish d : starterDishes){
+
                 String dishImageUri = drawable+d.getImage();
                 dishImageUri = dishImageUri.replace(".jpg", "");
                 String dishName = d.getName();
                 double dishCost = d.getPrice();
 
+                LinearLayout dishesView = (LinearLayout)view.findViewById(R.id.starter_choosing_dishes_id);
+
+
+                /*
+                //method of defining a new xml layout and resizing from there
+                LinearLayout dishView = (LinearLayout)view.findViewById(R.id.food_icon_id);
+
+                ImageButton imageButton = (ImageButton)view.findViewById(R.id.imageButton5);
+                int imageResource = view.getResources().getIdentifier(dishImageUri, null, view.getContext().getPackageName());
+                Drawable res = view.getContext().getResources().getDrawable(imageResource);
+                imageButton.setImageDrawable(res);
+
+                TextView dishText = (TextView)view.findViewById(R.id.textView20);
+                dishText.setText(dishName);
+
+                dishesView.addView(dishView);
+                */
+
+
+
+                //method of defining everything dynamically
+
+                LinearLayout container = new LinearLayout(view.getContext());
+                container.setOrientation(LinearLayout.HORIZONTAL);
+
+                FrameLayout imageholder = new FrameLayout(view.getContext());
+                container.addView(imageholder);
+                //imageholder.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                 //       ViewGroup.LayoutParams.MATCH_PARENT));
+                       // 30, 30));
+
+                ImageButton imageButton = new ImageButton(view.getContext());
+                imageholder.addView(imageButton);
+                int imageResource = view.getResources().getIdentifier(dishImageUri, null, view.getContext().getPackageName());
+                Drawable res = view.getContext().getResources().getDrawable(imageResource);
+                imageButton.setImageDrawable(res);
+                //imageButton.setLayoutParams(new ViewGroup.LayoutParams(
+                //        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                //imageButton.requestLayout();
+
+                TextView dishText = new TextView(view.getContext());
+                dishText.setText(dishName);
+                container.addView(dishText);
+
+                dishesView.addView(container);
+
+
+                //another method for not sure what
+                /*
                 int imageResource = view.getResources().getIdentifier(dishImageUri, null, view.getContext().getPackageName());
                 ImageButton imageButton= (ImageButton)view.findViewById(R.id.imageButton5);
                 Drawable res = view.getContext().getResources().getDrawable(imageResource);
                 imageButton.setImageDrawable(res);
+                */
 
             }
 
