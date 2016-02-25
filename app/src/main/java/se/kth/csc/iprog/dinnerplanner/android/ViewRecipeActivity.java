9cdbc2a08332797201c751c94ec65cc4269884f4
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-02-25 01:37:53 +0100

    done with a few bugs

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/ViewRecipeActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/ViewRecipeActivity.java
@@ -11,12 +11,15 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.IngredientsView;
+import se.kth.csc.iprog.dinnerplanner.android.view.RecipesView;
 import se.kth.csc.iprog.dinnerplanner.android.view.ViewRecipeBgView;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
 
 public class ViewRecipeActivity extends Activity {
 
     DinnerModel dinnerModel;
+    ViewGroup insertPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +32,69 @@ public class ViewRecipeActivity extends Activity {
         ImageButton mainsImage = (ImageButton)findViewById(R.id.imageButton3);
         ImageButton dessertImage = (ImageButton)findViewById(R.id.imageButton4);
 
+        insertPoint = (ViewGroup) findViewById(R.id.display_stuff_id);
+
         dinnerModel = ((DinnerPlannerApplication) this.getApplication()).getModel();
 
         ViewRecipeBgView viewRecipeBgView = new ViewRecipeBgView(findViewById(R.id.view_recipe_bg_view_id), dinnerModel);
+
 
         ingredientsImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
+                insertPoint.removeAllViews();
                 LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 View v = vi.inflate(R.layout.ingredients_view, null);
-                ViewGroup insertPoint = (ViewGroup) findViewById(R.id.display_stuff_id);
                 insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
                         ViewGroup.LayoutParams.FILL_PARENT));
-
                 IngredientsView ingredientsView = new IngredientsView(findViewById(R.id.ingredients_layout_id), dinnerModel);
 
+            }
+        });
+
+        starterImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                insertPoint.removeAllViews();
+                LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View v = vi.inflate(R.layout.recipes_view, null);
+                insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                        ViewGroup.LayoutParams.FILL_PARENT));
+                RecipesView recipesView = new RecipesView(findViewById(R.id.recipes_view_id), dinnerModel, Dish.STARTER);
+
+            }
+        });
+
+        mainsImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                insertPoint.removeAllViews();
+                LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View v = vi.inflate(R.layout.recipes_view, null);
+                insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                        ViewGroup.LayoutParams.FILL_PARENT));
+                RecipesView recipesView = new RecipesView(findViewById(R.id.recipes_view_id), dinnerModel, Dish.MAIN);
+
+            }
+        });
+
+        dessertImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                insertPoint.removeAllViews();
+                LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View v = vi.inflate(R.layout.recipes_view, null);
+                insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                        ViewGroup.LayoutParams.FILL_PARENT));
+                RecipesView recipesView = new RecipesView(findViewById(R.id.recipes_view_id), dinnerModel, Dish.DESERT);
 
             }
         });
--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/RecipesView.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/RecipesView.java
@@ -1,24 +1,63 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
 
+import android.graphics.Color;
 import android.text.Layout;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
 
 public class RecipesView {
 
     View view;
     DinnerModel dinnerModel;
+    int dishType;
 
-    public RecipesView(View view, DinnerModel model){
+    public RecipesView(View view, DinnerModel model, int type){
 
         this.view = view;
         this.dinnerModel = model;
+        this.dishType = type;
+
+        TextView title = (TextView)view.findViewById(R.id.textView17);
+        TextView dishName = (TextView)view.findViewById(R.id.textView18);
+        TextView dishInstructions = (TextView)view.findViewById(R.id.textView19);
+
+        Dish dishToDisplay = model.getSelectedDish(type);
+
+        if(dishToDisplay!=null){
+
+            if(dishType == Dish.STARTER){
+
+                title.setText("Starter");
+                title.setTextColor(Color.BLACK);
+
+            }else if(dishType == Dish.MAIN){
+
+                title.setText("Main Course");
+                title.setTextColor(Color.BLACK);
+
+            }else{
+
+                title.setText("Dessert");
+                title.setTextColor(Color.BLACK);
+
+            }
+
+            dishName.setText(dishToDisplay.getName());
+            dishName.setTextColor(Color.BLACK);
+
+            dishInstructions.setText(dishToDisplay.getDescription());
+            dishInstructions.setTextColor(Color.BLACK);
+
+        }
 
     }
 
